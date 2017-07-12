dae0ec4eedd046bf0acf78e9002d66dbb597fa7e
Adeyinka Adegbenro <adeyinkaadegbenro@gmail.com>
2017-07-12 18:34:31 +0000

    fixed port problem

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+var apikey = process.env.SLACK_API_TOKEN;
+
+app.get('/hello', (req,res)=>{
+    res.send('hello');
+});
+
+app.set('port', (process.env.PORT || 9001));
 
 app.post('/', (req, res) => {
   var text = req.body.text;
   // implement your bot here ...
-  console.log(text);
-  res.send("hello");
+       var body = {
+        response_type: "in_channel",
+        "attachments": [
+          {
+            "text": "hello world"
+          }
+        ]
+      };
+      res.send(body);
 });
 
 
-const server = app.listen(3000, () => {  
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-    
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
