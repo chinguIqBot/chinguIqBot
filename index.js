43c52b322839205b58eab7b1aaf147191016f930
Antonio Coppola <coppola.ant@gmail.com>
2017-08-06 00:01:26 +0100

    start commands setup

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+let apikey = process.env.API_KEY; //Slack API Key
+let hr_key = process.env.HR_KEY; //HackerRank API Key
+let hr_url = "http://api.hackerrank.com/checker/submission.json"; //HackerRank API URL
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-var apikey = process.env.SLACK_API_TOKEN;
 
 app.set('port', (process.env.PORT || 9001));
 
@@ -16,21 +19,37 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 })
 
-app.get('/hello', (req, res) => {
-    res.send('hello');
-});
-
-
 app.post('/', (req, res) => {
-    var text = req.body.text;
-    // implement your bot here ...
+    let payload = req.body;
+    let command = payload.text;
+
+    //Check KEY authorized
+    if (!apikey == payload.token) {
+        return;
+    }
+    //Send OK to slack to be able to send deferred answers later
+    res.end(200);
+
+    //Commands
+    if (command === "help") {
+
+    } else if (command.startsWith("topic")) {
+
+    } else if (command === "random") {
+
+    } else if (command === "challenge") {
+        
+    } else {
+
+    }
+    
     var body = {
         response_type: "in_channel",
         "attachments": [{
             "text": "hello world"
         }]
     };
-    res.send(body);
+    
 });
 
 
